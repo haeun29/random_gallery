b68bb66cf186843b69e63720560a7ac4d59dcf1b
haeun29 <kiha1110@naer.com>
2022-06-22 10:04:35 +0900

    test2

--- JS/app.js
+++ JS/app.js
@@ -1,58 +1,69 @@
 //이미지 변경 함수
 
-const img_url = [
- 'images/image1.jpg',
- 'images/image2.jpg',
- 'images/image3.jpg',
- 'images/image4.jpg',
- 'images/image5.jpg',
- 'images/image6.jpg'
-];
+    const img_url = [
+    'images/image1.jpg',
+    'images/image2.jpg',
+    'images/image3.jpg',
+    'images/image4.jpg',
+    'images/image5.jpg',
+    'images/image6.jpg'
+    ];
 
-//이미지 번호
-$(function (){
-
-    function changeBG(){
-//0~2 랜덤값 ( 이미지 번호 )
-    let imgNum = Math.floor(Math.random()*6);
-    console.log(imgNum);
-
-
-
-//이미지 변경하기
-    $('body').css({
-        background :    
-        `url(./images/blackBox.png),
-        url(${img_url[imgNum]})`
-
-    })
-    }
-
-    // setInterval(함수, 시간)
-    setInterval(function(){
-        changeBG();
-    },5000);
-
-    //연월일 시간 표시 
-    let d = new Date();
-    let year =d.getFullYear();
-    let month =  d.getMonth () + 1 ;
-    let day =  d.getDate();
+    //작가정보 출력하기 
+    const author = [
+        'Jabr Adalberht',
+        'Pallas Lancelot',
+        'Harmon Leann'
+    ];
     
-    console.log(year,month, day);
+    
 
-    $('.year').html(year);
-    $('.month').html('0'+month);
-    $('.day').html(day);
+    
 
-});
+    $(function(){
+
+        //연월일 시간 표시 
+        let d = new Date();
+        let year =d.getFullYear();
+        let month =  d.getMonth () + 1 ;
+        let day =  d.getDate();
+        // console.log(year,month, day);
+
+        //html 안에 날짜 입력
+        $('.year').html(year);
+        $('.month').html('0'+month);
+        $('.day').html(day);
+    
+
+         //0~6 랜덤값 ( 이미지 번호 )
+        function changeBG(){
+            let imgNum = Math.floor(Math.random()*6);
+            // console.log(imgNum);
+
+                //이미지 변경하기
+                $('body').css({
+                    background :    
+                    `url(./images/blackBox.png),
+                    url(${img_url[imgNum]})`
+
+                })
+
+                /// 작가정보 불러오기
+                $('.author span').html(author[imgNum]);
+
+        }
+        
+        $('.author span').html(author[0]);
+
+        setInterval(changeBG, 5000);
+
+    });
 
 
 
-//과제 : 이미지를 총 6장으로 확장하고 랜덤으로 돌리기
 
 
-
+//초기값
 
 
 
